efc4ee7d2e7f9f8917fb065f675e686c0c756f1b
valurkristinn <109678364+valurkristinn@users.noreply.github.com>
2026-03-13 01:37:32 +0000

    changes for loading and stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  prefetch: {
+    prefetchAll: true, // Automatically applies to standard links
+    defaultStrategy: "hover", // Uses the human gap between hovering and clicking
+  },
 });
